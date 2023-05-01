7e2007a55209932c4f3d07997c0f116c781c607b
maeva <maeva.80@hotmail.com>
2023-05-01 12:27:46 +0200

    can visualize one trip from mouscron to bxl

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from google.transit import gtfs_realtime_pb2
 import folium
 from google.protobuf.json_format import MessageToDict
 
-# todo: make a pin per trip
+
+# todo: make a pin per train
 # todo: segment railways on discord for better path
 # todo: add delays
 
@@ -30,32 +31,44 @@ def addLocations(stopId, line, positionsDict):
 def retrieveCoordinates():
     coordinates = []
     with open('sortedTravels.txt', 'r') as travels:
-        for line in travels:
-            longitude = float(line.split(',')[0])
-            latitude = float(line.split(',')[1])
-            epochTime = float(line.split(',')[2])
-            coordinates.append({"coordinates": [latitude, longitude],
-                                "time": datetime.fromtimestamp(float(epochTime)).isoformat() + "Z"})
-    return coordinates
+        for i, line in enumerate(travels):
+            if i > 0:
+                longitude = float(line.split(',')[0])
+                latitude = float(line.split(',')[1])
+                epochTime = float(line.split(',')[2])
+                coordinates.append({"coordinates": [latitude, longitude],
+                                    "time": datetime.fromtimestamp(float(epochTime)).isoformat() + "Z"})
+        return coordinates
 
 
 def sortLines():
-    # Open the input and output files
-    with open('unsortedTravels.txt', 'r') as f_input, open('sortedTravels.txt', 'w') as f_output:
-        # Read the lines from the input file
+    with open('unsortedTravels.txt', 'r') as f_input:
         lines = f_input.readlines()
+        lines = [line.strip().split(',') for line in lines]  # split each line into a list of values
 
-        # Sort the lines based on the last parameter
-        sorted_lines = sorted(lines, key=lambda line: int(line.split(',')[-1]))
+        sortedLines = sorted(lines, key=lambda line: int(line[-1]))  # sort the lines based on the epoch value
 
-        # Write the sorted lines to the output file
-        f_output.writelines(sorted_lines)
+        uniqueLines = []  # create an empty list to store the unique lines
+        prevLine = None  # initialize prevLine variable to None
+        for line in sortedLines:
+            if line != prevLine:  # check if the line is different from the previous line
+                uniqueLines.append(line)  # add the line to the uniqueLines list
+                prevLine = line  # set the prevLine variable to the current line
+
+    return uniqueLines
+
+
+def writeSortedTravels():
+    uniqueLines = sortLines()
+    with open('sortedTravels.txt', 'a+') as f_output:
+        for line in uniqueLines:
+            f_output.write(','.join(line) + '\n')
 
 
 def createGeoJSON():
     coordinates = retrieveCoordinates()
     print(coordinates)
-    # Create a GeoJSON FeatureCollection with a LineString feature
+
     feature_collection = {
         "type": "FeatureCollection",
         "features": [{
@@ -118,7 +131,7 @@ def locateTrain(speed, stations, distanceStations, currentTime):
 
 def visualizeTrains():
     belgium_coords = [51.17147, 4.142963]
-    m = folium.Map(location=belgium_coords, zoom_start=10)
+    m = folium.Map(location=belgium_coords, zoom_start=8)
 
     feature_collection = createGeoJSON()
 
@@ -136,6 +149,24 @@ def visualizeTrains():
     webbrowser.open('map.html')
 
 
+def writeStaticObjectCoordinates(i, previousPosition, stations):
+    distanceStations = calculateDistance(stations)
+    speed = calculateAverageSpeed(stations, distanceStations)
+    previousPosition = [locateTrain(speed, stations, distanceStations, i)]
+    with open('unsortedTravels.txt', 'a+') as travels:
+        travels.write(f"{previousPosition[0][0]},{previousPosition[0][1]},{i}\n")
+        travels.close()
+    return previousPosition
+
+
+def writeMovingobjectCoordinates(i, previousPosition, stations):
+    with open('unsortedTravels.txt', 'a+') as travels:
+        travels.write(f"{stations[0][1]},{stations[0][2]},{i}\n")
+        travels.close()
+    previousPosition = [stations[0][1], stations[0][2]]
+    return previousPosition
+
+
 class gtfsData:
     def __init__(self):
         url = "file:///C:/Users/maeva/Desktop/geo/1680127355.gtfsrt"
@@ -149,36 +180,43 @@ class gtfsData:
         visualizeTrains()
 
     def findTrainLocation(self):
+        startTime = self.findStartTime()
+        endTime = self.gtfsValues[5]['tripUpdate']['stopTimeUpdate'][-1]['arrival']['time']
         previousPosition = None
-        for i in range(1680048000, 1680134400, 30):
-            stations = self.findStations(i)
+        previousTripId = None
+        for i in range(startTime, int(endTime), 60):
+            stations, previousTripId = self.findStations(i, previousTripId)
 
             if len(stations) == 1 and [stations[0][1], stations[0][2]] != previousPosition:
-                with open('unsortedTravels.txt', 'a+') as travels:
-                    travels.write(f"{stations[0][1]},{stations[0][2]},{i}\n")
-                    travels.close()
-                previousPosition = [stations[0][1], stations[0][2]]
+                previousPosition = writeMovingobjectCoordinates(i, previousPosition, stations)
 
             elif len(stations) > 1:
-                distanceStations = calculateDistance(stations)
-                speed = calculateAverageSpeed(stations, distanceStations)
-                with open('unsortedTravels.txt', 'a+') as travels:
-                    travels.write(f"{stations[0][1]},{stations[0][2]},{i}\n")
-                    travels.close()
-                previousPosition = [locateTrain(speed, stations, distanceStations, i)]
-        sortLines()
+                previousPosition = writeStaticObjectCoordinates(i, previousPosition, stations)
 
-    def findStations(self, currentTime):
+        writeSortedTravels()
+
+    def findStations(self, currentTime, previousTripId):
         stops = self.gtfsValues[5]['tripUpdate']['stopTimeUpdate']
         for i in range(len(stops) - 1):
+            if self.gtfsValues[5]['tripUpdate']['trip']['tripId'] != previousTripId:
+                previousTripId = self.gtfsValues[5]['tripUpdate']['trip']['tripId']
+
             if int(stops[i]['departure']['time']) < currentTime < int(stops[i + 1]['arrival']['time']):
                 return [self.positionsDict[stops[i]['stopId']], self.positionsDict[stops[i + 1]['stopId']],
-                        stops[i]['departure']['time'], stops[i + 1]['arrival']['time']]
+                        stops[i]['departure']['time'], stops[i + 1]['arrival']['time']], previousTripId
+
             elif i < len(stops) - 2:
                 if int(stops[i + 1]['departure']['time']) >= currentTime >= int(stops[i + 1]['arrival']['time']):
-                    return [self.positionsDict[stops[i + 1]['stopId']]]
+                    return [self.positionsDict[stops[i + 1]['stopId']]], previousTripId
             else:
-                return [self.positionsDict[stops[i + 1]['stopId']]]
+                return [self.positionsDict[stops[i + 1]['stopId']]], previousTripId
+
+    def findStartTime(self):
+        startTime = self.gtfsValues[5]['tripUpdate']['trip']['startTime']
+        startDate = self.gtfsValues[5]['tripUpdate']['trip']['startDate']
+        startDateTime = datetime.strptime(startDate + startTime, '%Y%m%d%H:%M:%S')
+        epochTime = int(startDateTime.timestamp())
+        return epochTime
 
     def visualizeStations(self):
         belgium_coords = [50.5039, 4.4699]
