6f88373c0fec89946334585733f5b630eaa51e4a
maedekey <45097036+maedekey@users.noreply.github.com>
2023-05-06 23:07:06 +0200

    Add files via upload
    now we can visualize a trip by entering source and dest and time

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import psycopg2
-import visualization
 import os
+import common
 
 """
 sudo -u postgres psql
@@ -52,49 +52,102 @@ def printTable(conn, cur):
         print(row)
 
 
-def loadGtfsData(cur, url):
-    positionsDict = visualization.findStationPositions()
-    gtfsDict = visualization.preprocessing(url)
+def loadGtfsData(cur, url, counter):
+    positionsDict = common.findStationPositions()
+    gtfsDict = common.preprocessing(url)
     if gtfsDict:
         if len(gtfsDict) == 2:
             gtfsValues = list(gtfsDict.values())[1]
             if gtfsValues:
                 for i in range(len(gtfsValues)):
-                    res = findStations(gtfsValues, positionsDict, i)
+                    res = findStations(gtfsValues, positionsDict, i, counter)
                     if res:
+                        print(counter)
+                        counter += 1
                         for station in res:
                             insertInTable(cur, station)
+    return counter
 
 
 def importGtfsData(cur):
     dataPath = "data/"
     files = os.listdir(dataPath)
-
+    files = [files[0], files[1]]
+    counter = 0
     for file in files:
         cwd = os.getcwd()
         full_path = os.path.join(cwd, dataPath+file)
         url = 'file://' + full_path
         if url.endswith(".gtfsrt"):
-            loadGtfsData(cur, url)
+            counter = loadGtfsData(cur, url, counter)
 
 
-def findStations(gtfsValues, positionsDict, trip):
+def findStations(gtfsValues, positionsDict, trip, counter):
     res = []
     stops = gtfsValues[trip]['tripUpdate']['stopTimeUpdate']
     if len(stops) > 1:
         for i in range(len(stops)):
             if i == len(stops)-1:
                 res.append([positionsDict[stops[i]['stopId']],
-                            stops[i]['arrival']['time'], trip])
+                            stops[i]['arrival']['time'], counter])
+
             else:
                 if 'departure' not in stops[i] and i > 0:
                     stops[i]['departure'] = {'delay': stops[i - 1]['departure']['delay'],
                                              'time': stops[i - 1]['departure']['time']}
                 if 'departure' in stops[i]:
                     res.append([positionsDict[stops[i]['stopId']],
-                                stops[i]['departure']['time'], trip])
+                                stops[i]['departure']['time'], counter])
 
         return res
 
 
-main()
+#main()
+
+
+def retrievePath(conn, trip):
+    cur = conn.cursor()
+    cur.execute("select * from station where trip = %s", (trip,))
+    conn.commit()
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+
+def retrieveStations(arrivalStation, departureStation):
+    stationDico = {}
+    for station in departureStation:
+        keyTrip = station[-1]
+        if keyTrip in stationDico:
+            stationDico[keyTrip].append(station)
+        else:
+            stationDico[keyTrip] = [station]
+    validDepartureStation = []
+    validArrivalStation = []
+    for station in arrivalStation:
+        keyTrip = station[-1]
+        if keyTrip in stationDico:
+            validDepartureStation.append(stationDico[keyTrip])
+            validArrivalStation.append(station)
+    return validArrivalStation, validDepartureStation
+
+
+def retrieveArrivalStation(conn, station, epoch):
+    cur = conn.cursor()
+    cur.execute("select * from station where nameStation = %s and arrivalTime > %s", (station, epoch,))
+    conn.commit()
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+
+def retrieveDepartureStation(conn, station, epoch):
+    cur = conn.cursor()
+    cur.execute("select * from station where nameStation = %s and arrivalTime < %s", (station, epoch,))
+    conn.commit()
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+
+
